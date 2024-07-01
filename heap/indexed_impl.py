d5e5d9dea3692965083fc7a737b0673509721388
Praveen Perumal <ppraveen98841@gmail.com>
2024-07-01 07:39:40 +0530

    Update IndexedHeap

--- heap/indexed_impl.py
+++ heap/indexed_impl.py
@@ -1,100 +1,96 @@
-class IndexedMinHeap:
-    def __init__(self, maxindex) -> None:
-        self.maxindex = maxindex  # index belongs to [0, maxindex]
+class IndexedHeap:
+    """
+    Convension: i, j are node index and ki, kj are key index
+    """
+    def __init__(self, N):
+        self.N = N
         self.size = 0
-        self.tree: list[int] = [None for _ in range(maxindex + 1)]  # heap stores index # type: ignore
-        self.loc: list[int] = [None for _ in range(maxindex + 1)]  # location of a particular index in the heap # type: ignore
-        self.key = [None for _ in range(maxindex + 1)]  # index-key table # type: ignore
 
-    def child(self, i):
-        l = 2*i + 1 if 2*i + 1 < self.size else None
-        r = 2*i + 2 if 2*i + 2 < self.size else None
-        return l, r
+        self.values = [None] * N     # key index => value
+        self.pm = [None] * N        # position map: key index => node index
+        self.im = [None] * N        # inverse map: node index => key index
 
-    def min_child(self, i):
-        l, r = self.child(i)
-        if l and r:
-            return r if self.key[self.tree[r]] < self.key[self.tree[l]] else l
-        return l
+    def less(self, i, j):
+        return self.values[self.im[i]] < self.values[self.im[j]]
 
-    def parent(self, i):
-        return (i-1) // 2 if i > 0 else None
+    def swap(self, i, j):
+        ki = self.im[i]
+        kj = self.im[j]
+        self.im[i] = kj
+        self.im[j] = ki
+        self.pm[ki] = j
+        self.pm[kj] = i
 
-    def bubble_up(self, i):
-        j = self.parent(i)
-        while j is not None:
-            if self.key[self.tree[j]] > self.key[self.tree[i]]:
-                self.tree[i], self.tree[j] = self.tree[j], self.tree[i]
-                self.loc[self.tree[i]], self.loc[self.tree[j]] = self.loc[self.tree[i]], self.loc[self.tree[j]]
-                i = j
-                j = self.parent(i)
-            else:
+    def swin(self, i):
+        p = (i-1) // 2
+        while p >= 0:
+            if self.less(p, i):
                 break
+            self.swap(i, p)
+            i = p
+            p = (i-1) // 2
 
-    def bubble_down(self, i):
-        j = self.min_child(i)
-        while j is not None:
-            if self.key[self.tree[i]] > self.key[self.tree[j]]:
-                self.tree[i], self.tree[j] = self.tree[j], self.tree[i]
-                self.loc[self.tree[i]], self.loc[self.tree[j]] = self.loc[self.tree[i]], self.loc[self.tree[j]]
-                i = j
-                j = self.min_child(i)
-            else:
+    def sink(self, i):
+        left = 2*i + 1
+        right = 2*i + 2
+        minimum = right if right < self.size and self.less(right, left) else left
+
+        while minimum < self.size:
+            if self.less(i, minimum):
                 break
+            self.swap(i, minimum)
 
-    def push(self, i, key):
-        if not (0 <= i <= self.maxindex):
-            raise Exception("Index exceeds maxindex")
+            i = minimum
+            left = 2*i + 1
+            right = 2*i + 2
+            minimum = right if right < self.size and self.less(right, left) else left
 
-        if self.loc[i] is not None:
-            raise Exception("Index already exist")
-
-        self.tree[self.size] = i
-        self.loc[i] = self.size
-        self.key[i] = key
+    def insert(self, ki, value):
+        i = self.size
+        self.values[ki] = value
+        self.im[i] = ki
+        self.pm[ki] = i
         self.size += 1
-        self.bubble_up(self.size - 1)
+        self.swin(i)
+
+    def remove(self, ki):
+        i = self.pm[ki]
+        j = self.size - 1
+        self.swap(i, j)
+
+        self.values[ki] = None
+        self.pm[ki] = None
+        self.im[j] = None 
+        self.size -= 1
+
+        self.swin(i)
+        self.sink(i)
+
+
+    def update(self, ki, value):
+        self.values[ki] = value
+        i = self.pm[ki]
+        self.swin(i)
+        self.sink(i)
+
+    def decreaseKey(self, ki, value):
+        if value < self.values[ki]:
+            self.values[ki] = value
+            self.swin(self.pm[ki])
+
+    def increaseKey(self, ki, value):
+        if value > self.values[ki]:
+            self.values[ki] = value
+            self.sink(self.pm[ki])
+
+    def poll(self):
+        if self.size == 0:
+            return None
+        return (self.im[0], self.values[self.im[0]])
 
     def pop(self):
         if self.size == 0:
             return None
-
-        i = self.tree[0]
-        key = self.key[i]
-        self.key[i] = None
-        self.loc[i] = None # type: ignore
-
-        self.tree[0] = self.tree[self.size - 1]
-        self.loc[self.tree[self.size - 1]] = 0
-        self.tree[self.size - 1] = None # type: ignore
-
-        self.size -= 1
-        if self.size > 0:
-            self.bubble_down(0)
-        return (i, key)
-
-    def setKey(self, i, key):
-        if self.key[i] is None:
-            raise Exception("Index doesn't exist")
-
-        old_key = self.key[i]
-        self.key[i] = key
-        if key > old_key:
-            self.bubble_down(self.loc[i])
-        elif key < old_key:
-            self.bubble_up(self.loc[i])
-
-    def getKey(self, i):
-        if self.key[i] is None:
-            raise Exception("Index doesn't exist")
-        return self.key[i]
-
-    def top(self):
-        return (self.tree[0], self.key[self.tree[0]]) if self.tree else None
-
-    def __len__(self):
-        return self.size
-
-    def __contains__(self, i):
-        return self.loc[i] is not None
-
+        key, value = self.poll()
+        self.remove(key)
+        return key, value
